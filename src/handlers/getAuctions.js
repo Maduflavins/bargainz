7a95c96e8704b6c8c5ad14375d6d3229d638c9cf
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-06-02 17:10:36 +0100

    json validation: added json validation schema for getAuctions

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,17 +1,19 @@
 import AWS from 'aws-sdk'
 import commonMiddleware from '../lib/commonMiddleware'
 import createError from 'http-errors';
-
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema'
+import validator from '@middy/validator'
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context){
     let auctions;
 
-    const { status } = event.querryStringParameters;
+    const { status } = event.queryStringParameters;
     
     const params = {
       TableName: process.env.AUCTIONS_TABLE_NAME,
-      keyConditionExpression: '#status = :status',
+      IndexName: 'statusAndEndDate',
+      KeyConditionExpression: '#status = :status',
       ExpressionAttributeValues: {
         ':status': status,
       },
@@ -36,6 +38,7 @@ async function getAuctions(event, context){
 }
 
 export const handler = commonMiddleware(getAuctions)
+  .use(validator({ inputSchema: getAuctionsSchema, useDefaults: true}))
   
 
 
